5d42ee8bab91b2fb32d2b84769a815fbc0bda4c7
AndresGarciaEscalante <andres_ge@hotmail.es>
2023-11-01 20:09:07 -0600

    fix: Solver memory segmentation fault

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -11,7 +11,7 @@ GraphNode::~GraphNode()
     //// STUDENT CODE
     ////
 
-    delete _chatBot; 
+    //delete _chatBot; This line of code seems to not have a purpose
 
     ////
     //// EOF STUDENT CODE
